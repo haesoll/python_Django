bfdedee070bfbb57d64a6b1fb40819df529d3ff8
haesoll <cdouble172@gmail.com>
2020-10-29 19:48:35 +0900

    init

---
+++ my_setting.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_test',
+        'USER': 'root',
+        'PASSWORD': 'cass94',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
